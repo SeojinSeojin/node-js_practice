a68e8d3df9155b58a498de3942072fa8a829fed6
SeojinSeojin <1106laura@naver.com>
2021-02-23 17:39:03 +0900

    github api with ghrepos

--- github_api/app.js
+++ github_api/app.js
@@ -1,12 +1,41 @@
+require("dotenv").config();
+
 var GitHub = require('github-api');
 
 // no auth
-var gh = new GitHub();
 
-var seojinseojin = gh.getUser('seojinseojin');
-console.log(seojinseojin)
+
+var noAuth = new GitHub();
+var seojinseojin = noAuth.getUser('seojinseojin');
 seojinseojin.listStarredRepos(function(err, repos) {
     repos.forEach((repo) => {
         console.log(`" ${repo.name} " by  "${repo.owner.login}"`)
     })
 });
+
+const ghrepos = require("ghrepos");
+const authOptions = { use: "seojinseojin", token: process.env.PAT }
+
+
+ghrepos.listUser(authOptions, "seojinseojin", (err, repolist) => {
+    repolist.forEach((repo) => {
+        console.log(`${repo.name} - ${repo.description} by  ${repo.owner.login}, ${repo.language}`);
+    })
+})
+
+ghrepos.listUser(authOptions, "seojinseojin", (err, repolist) => {
+    repolist.forEach((repo) => {
+        ghrepos.listCommits(authOptions, "seojinseojin", repo.name, (err, refData) => {
+            console.log(`\nRepository : ${repo.name} by ${repo.owner.login}`)
+            refData.forEach((ref) => {
+                console.log(`${ref.commit.author.date}\n${ref.commit.committer.name} committed : ${ref.commit.message}`)
+            })
+        })
+    })
+})
+
+ghrepos.listUser(authOptions, "seojinseojin", (err, repolist) => {
+    repolist.forEach((repo) => {
+        console.log(`${repo.name} - ${repo.languages_url}`);
+    })
+})
